5257cad8625ae8eb05fa27e416ae9e77a1fde37f
Clem <clemtrichetstg@gmail.com>
2023-10-30 16:28:20 +0100

    Mob creation/Main scene

--- Player.gd
+++ Player.gd
@@ -1,4 +1,5 @@
-extends Sprite2D
+extends Area2D
+signal hit
 
 @export var speed = 400 #nombre de pixel dont ce deplace le joueur
 var screen_size
@@ -6,6 +7,7 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -21,6 +23,26 @@ func _process(delta):
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed;
+		$AnimatedSprite2D.play()
+	else :
+		$AnimatedSprite2D.stop()
 	
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+
+
+func _on_body_entered(body): #Function lorsque le player touche un ennemeies
+	hide() #Cache le player
+	hit.emit() 
+	$CollisionShape2D.set_deferred("disabled", true) #On desactive le noeud lier à la box de collision
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
